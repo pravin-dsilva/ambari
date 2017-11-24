d87ce0bb45e7ad2c03a3853d9bf1c872c6dc01d2
Pravin DSilva <pravin_dsilva@persistent.co.in>
2017-11-24 11:37:48 +0530

    AMBARI-22397: Verify JAVA_HOME and prompt user to enter JAVA_HOME for new OS family

--- ambari-web/test/controllers/wizard/step3_test.js
+++ ambari-web/test/controllers/wizard/step3_test.js
@@ -2567,6 +2567,7 @@ describe('App.WizardStep3Controller', function () {
       var expected = {
           name: 'name',
           home: 'home',
+          ostypehome: 'ab',
           location: 'location'
         },
         data = {
@@ -2574,15 +2575,25 @@ describe('App.WizardStep3Controller', function () {
             properties: {
               'jdk.name': expected.name,
               'java.home': expected.home,
-              'jdk_location': expected.location
+              'jdk_location': expected.location,
+              'java.home.aa': expected.ostypehome
             }
           }
         };
-
+      var hostData = {
+          href: 'abc',
+          items: [
+                  {Hosts: {host_name: 'h1', os_type: 'aa'}}
+                  ]
+      };
+      c.set('jsonHostData', hostData);
       c.getJDKNameSuccessCallback(data);
       expect(c.get('needJDKCheckOnHosts')).to.equal(false);
       expect(c.get('jdkLocation')).to.equal(expected.location);
       expect(c.get('javaHome')).to.equal(expected.home);
+      var javaHomeProperty = c.get('javaHomePropertyList');
+      expect(javaHomeProperty.length).to.equal(1);
+      expect(javaHomeProperty[0].java_home_property).to.equal("ab");
     });
 
   });
@@ -2718,7 +2729,17 @@ describe('App.WizardStep3Controller', function () {
           }
         ]
       };
-
+      var hostData = {
+          href: 'abc',
+          items: [
+                  {Hosts: {host_name: 'h1', os_type: 'aa'}},
+                  {Hosts: {host_name: 'h2', os_type: 'ab'}}
+                  ]
+      };
+      c.set('jsonHostData', hostData);
+      var javaHomeProp = Em.A([{os_type: 'aa', java_home_property: '/a/b'}]);
+      c.set('javaHomePropertyList', javaHomeProp);
+      c.set('javaHomeNewOs', Em.A([]));
       c.set('jdkCategoryWarnings', {});
       c.parseJDKCheckResults(data);
       var result = c.get('jdkCategoryWarnings');
@@ -2727,7 +2748,117 @@ describe('App.WizardStep3Controller', function () {
 
     });
 
+    it('should display java home prompt if jdk warnings exist', function () {
+      var data = {
+          Requests: {
+            end_time: 1
+          },
+          tasks: [
+                  {
+                    Tasks: {
+                      host_name: 'h1',
+                      structured_out: {
+                        java_home_check: {
+                          exit_code: 1
+                        }
+                      }
+                    }
+                  },
+                  {
+                    Tasks: {
+                      host_name: 'h2',
+                      structured_out: {
+                        java_home_check: {
+                          exit_code: 0
+                        }
+                      }
+                    }
+                  }
+                  ]
+      };
+
+      c.set('javaHomeNewOs', Em.A([]));
+      var hostData = {
+          href: 'abc',
+          items: [
+                  {Hosts: {host_name: 'h1', os_type: 'aa'}},
+                  {Hosts: {host_name: 'h2', os_type: 'ab'}}
+                  ]
+      };
+      c.set('jsonHostData', hostData);
+      var javaHomeProp = Em.A([{os_type: 'aa', java_home_property: null}]);
+      c.set('javaHomePropertyList', javaHomeProp);
+      c.parseJDKCheckResults(data);
+      var javaHomeNewOsResult = c.get('javaHomeNewOs');
+      expect(javaHomeNewOsResult.length).to.equal(1);
+    });
   });
+
+  describe('#displayJavaHomePanel', function () {
+    beforeEach(function () {
+      var javaHome = Em.A([{value: '', os_type: 'aa'}]);
+      c.set('javaHomeNewOs', javaHome);
+      c.set('displayJavaHomePrompt',false);
+    });
+    it('should not display the java home prompt if the property is already set', function () {
+      var javaHomeProp = Em.A([{os_type: 'aa', java_home_property: '/a/b'}]);
+      c.set('javaHomePropertyList', javaHomeProp);
+      c.displayJavaHomePanel();
+      expect(c.get('displayJavaHomePrompt')).to.equal(false);
+    });
+    it('should  display the java home prompt if the property is not set', function () {
+      var javaHomeProp = Em.A([{os_type: 'aa', java_home_property: null}]);
+      c.set('javaHomePropertyList', javaHomeProp);
+      c.displayJavaHomePanel();
+      expect(c.get('displayJavaHomePrompt')).to.equal(true);
+    });
+  });
+
+  describe('#notifyJDKErrorExists', function () {
+    beforeEach(function () {
+      var javaHome = Em.A([{value: '', os_type: 'aa'}]);
+      c.set('javaHomeNewOs', javaHome);
+      c.set('displayJavaHomeError',false);
+    });
+    it('should  display the error message ', function () {
+      var javaHomeTmp = Em.A([{os_type: 'aa'}]);
+      c.set('javaHomeNewOsTmp', javaHomeTmp);
+      c.notifyJDKErrorExists();
+      expect(c.get('displayJavaHomeError')).to.equal(true);
+    });
+    it('should not display the error message', function () {
+      var javaHomeTmp = Em.A([]);
+      c.set('javaHomeNewOsTmp', javaHomeTmp);
+      c.notifyJDKErrorExists();
+      expect(c.get('displayJavaHomeError')).to.equal(false);
+    });
+  });
+
+    describe('#getHostOsInfo', function () {
+      it('should do ajax request', function () {
+        c.getHostOsInfo();
+        var args = testHelpers.findAjaxRequest('name', 'wizard.step3.host_info');
+        expect(args).exists;
+      });
+    });
+
+    describe('#getHostOsInfoSuccessCallback', function () {
+      it('should get the correct data', function(){
+        c.set('jsonHostData',{});
+        var data = {items: [{Hosts: {host_name: 'h1'}} ]};
+        var resolve = false;
+        var params = {
+            dfd: {
+              resolve: function() {
+                resolve = true;
+              }
+            }
+        };
+        c.getHostOsInfoSuccessCallback(data ,null, params);
+        expect(resolve).to.be.true;
+        expect(c.get('jsonHostData')).to.be.eql(data);
+      });
+    });
 
   describe('#getHostCheckTasksSuccess', function() {
 
